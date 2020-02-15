b473082349779b224e21187a5865a47063ef2a92
Jonathan Lemus <lemusj27@hotmail.com>
2020-02-15 04:43:28 -0500

    Added Help Menu and items for our GitHubs Links are a WIP

--- src/view/MenuBox.java
+++ src/view/MenuBox.java
@@ -1,21 +1,33 @@
 package view;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.control.DialogEvent;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
 public class MenuBox {
 	private MenuBar menuBar;
-	private Menu menuFile, menuView;
-	private MenuItem miExit, miConfirmedCases, miDeaths, miRecoveries;
+	private Menu menuFile, menuView, menuHelp;
+	private MenuItem miExit, miConfirmedCases, miDeaths, miRecoveries, miMichaelGH, 
+	miKieferGH, miChrisGH, miJonathanGH, miCDC, miSources, miAbout;
 	private LineChart chart;
-	
+	private WebView browser = new WebView();
+	private WebEngine appBrowser = browser.getEngine();
+
 	public MenuBox(BorderPane pane) {
 		menuBar = new MenuBar();		
 		menuFile = new Menu("File");
@@ -26,14 +38,27 @@ public class MenuBox {
 		miDeaths = new MenuItem("Deaths");
 		miRecoveries = new MenuItem("Recoveries");
 		
+		menuHelp = new Menu("Help");
+		miMichaelGH = new MenuItem("Michael's GitHub");
+		miKieferGH = new MenuItem("Kiefer's GitHub");
+		miChrisGH = new MenuItem("Chris's GitHub");
+		miJonathanGH = new MenuItem("Jonathan's GitHub");
+		miCDC = new MenuItem("CDC");
+		miSources = new MenuItem("Sources");
+		miAbout = new MenuItem("About");
+		
 		pane.setTop(this.getRoot());
 		
 		menuFile.getItems().add(miExit);
 		menuView.getItems().addAll(miConfirmedCases, new SeparatorMenuItem(),
 				miDeaths, new SeparatorMenuItem(),
 				miRecoveries);
+		menuHelp.getItems().addAll(miMichaelGH, new SeparatorMenuItem(),
+				miKieferGH, new SeparatorMenuItem(), miChrisGH, new SeparatorMenuItem(), 
+				miJonathanGH, new SeparatorMenuItem(), miCDC, new SeparatorMenuItem(),
+				miSources, new SeparatorMenuItem(), miAbout);
 		
-		menuBar.getMenus().addAll(menuFile, menuView);
+		menuBar.getMenus().addAll(menuFile, menuView, menuHelp);
 		
 		chart = makeChart();
 		
@@ -59,6 +84,27 @@ public class MenuBox {
 		miRecoveries.setOnAction(e -> {
 			pane.setCenter(recoveryBox);
 		});
+		
+		miMichaelGH.setOnAction(e -> {
+			appBrowser.load("https://github.com/Camposm97");
+		});
+		
+		miKieferGH.setOnAction(e -> {
+			appBrowser.load("https://github.com/kiefuh");
+		});
+		
+		miChrisGH.setOnAction(e -> {
+			appBrowser.load("https://github.com/The3FacesOfSteve");
+		});
+		
+		miJonathanGH.setOnAction(e -> {
+			appBrowser.load("https://github.com/LemusJ27");
+		});
+		
+		miAbout.setOnAction(e -> {
+			
+		});
+		
 	}
 	
 	public MenuBar getRoot() {
