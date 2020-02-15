10de425aa6453cec29c7e595dead1ba48f65e582
Camposm97 <camposm1205@gmail.com>
2020-02-15 00:34:20 -0500

    Carrying

--- src/util/CamposUtils.java
+++ src/util/CamposUtils.java
@@ -5,18 +5,16 @@ import java.util.Arrays;
 
 public class CamposUtils {
 	public static void main(String[] args) {
-		String dir = "res/archived_data/daily_case_updates";
-		File[] arr = scanFiles(dir);
-		System.out.println(Arrays.toString(arr));
-		for(int i = 0; i < arr.length; i++) {
-			File file = arr[i];
-			if (file.isFile()) {
-				System.out.println(file.getName());			
-			}
-		}
+		String s = ",,,";
+//		System.out.println(Arrays.toString(s.split(", ")));
+		System.out.println(s.replaceAll(",", ", "));
 	}
 	
 	public static File[] scanFiles(String dir) {
 		return new File(dir).listFiles();
+	}
+	
+	public static String editCommas(String s) {
+		return s.replaceAll(",", ", ");
 	}
 }
