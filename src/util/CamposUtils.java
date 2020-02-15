a3918c37e70e0485e9640b02cde0d830bd24f0ee
Camposm97 <camposm1205@gmail.com>
2020-02-14 20:21:47 -0500

    Modified CamposUtils

--- src/util/CamposUtils.java
+++ src/util/CamposUtils.java
@@ -1,7 +1,18 @@
 package util;
 
+import java.io.File;
+import java.util.Arrays;
+
 public class CamposUtils {
 	public static void main(String[] args) {
-		
+		File folder = new File("res/archived_data/daily_case_updates");
+		File[] arr = folder.listFiles();
+		System.out.println(Arrays.toString(arr));
+		for(int i = 0; i < folder.length() - 1; i++) {
+			File file = arr[i];
+			if (file.isFile()) {
+				System.out.println(file.getName());			
+			}
+		}
 	}
 }
