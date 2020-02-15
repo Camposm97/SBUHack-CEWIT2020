03e5b9206ce5ca0737a3d176569c0d0799a29d7c
Camposm97 <camposm1205@gmail.com>
2020-02-15 15:04:35 -0500

    Created MenuCorona

---
+++ src/view/MenuCorona.java
@@ -0,0 +1,55 @@
+package view;
+
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+public class MenuCorona extends Menu {
+	private MenuItem miConfirmedCases, miDeaths, miRecoveries;
+	private LineChart chartConfirmed, chartDeath, chartRecovery;
+
+	
+	public MenuCorona(BorderPane root) {
+		super("Coronavirus");
+		
+		miConfirmedCases = new MenuItem("Confirmed Cases");
+		miDeaths = new MenuItem("Deaths");
+		miRecoveries = new MenuItem("Recoveries");
+		
+		chartConfirmed = makeChart();
+		chartDeath = makeChart();
+		chartRecovery = makeChart();
+		
+		ConfirmedBox bigCBox = new ConfirmedBox(chartConfirmed);
+		VBox confirmedBox = bigCBox.getConfirmedBox();
+		DeathBox bigDBox = new DeathBox(chartDeath);
+		VBox deathBox = bigDBox.getDeathBox();
+		RecoveryBox bigRBox = new RecoveryBox(chartRecovery);
+		VBox recoveryBox = bigRBox.getRecoveryBox();
+		
+		miConfirmedCases.setOnAction(e -> {
+			root.setCenter(confirmedBox);
+		});
+
+		miDeaths.setOnAction(e -> {
+			root.setCenter(deathBox);
+		});
+
+		miRecoveries.setOnAction(e -> {
+			root.setCenter(recoveryBox);
+		});
+		this.getItems().addAll(miConfirmedCases, miDeaths, miRecoveries);
+	}
+	
+	public LineChart makeChart() {
+		NumberAxis xAxis = new NumberAxis();
+		xAxis.setLabel("Days");
+		NumberAxis yAxis = new NumberAxis();
+		yAxis.setLabel("Cases");
+		LineChart chart = new LineChart(xAxis, yAxis);
+		return chart;
+	}
+}
--- src/view/MyMenuBar.java
+++ src/view/MyMenuBar.java
@@ -2,25 +2,17 @@ package view;
 
 import app.App;
 import javafx.application.Platform;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
 import util.AlertHelper;
 import util.CamposWeb;
 
 public class MyMenuBar extends MenuBar {
 	private BorderPane root;
-	private Menu menuFile, menuView, menuHelp, menuGithub;
-	private MenuItem miExit, miConfirmedCases, miDeaths, miRecoveries;
-	private MenuItem miCDC, miDonate, miSources, miAbout;
-	private LineChart chartConfirmed, chartDeath, chartRecovery;
+	private Menu menuFile, menuView, menuCorona, menuHelp, menuGithub;
+	private MenuItem miExit, miCDC, miDonate, miSources, miAbout;
 
 	public MyMenuBar(BorderPane root) {
 		this.root = root;
@@ -33,10 +25,8 @@ public class MyMenuBar extends MenuBar {
 		menuFile = new Menu("File");
 		miExit = new MenuItem("Exit");
 		menuView = new Menu("View");
+		menuCorona = new MenuCorona(root);
 
-		miConfirmedCases = new MenuItem("Confirmed Cases");
-		miDeaths = new MenuItem("Deaths");
-		miRecoveries = new MenuItem("Recoveries");
 		menuGithub = new MenuGithub();
 
 		menuHelp = new Menu("Help");
@@ -44,34 +34,13 @@ public class MyMenuBar extends MenuBar {
 		miDonate = new MenuItem("Donate");
 		miSources = new MenuItem("Sources");
 		miAbout = new MenuItem("About");
-
-		chartConfirmed = makeChart();
-		chartDeath = makeChart();
-		chartRecovery = makeChart();
 	}
 
 	private void editControls() {
-		ConfirmedBox bigCBox = new ConfirmedBox(chartConfirmed);
-		VBox confirmedBox = bigCBox.getConfirmedBox();
-		DeathBox bigDBox = new DeathBox(chartDeath);
-		VBox deathBox = bigDBox.getDeathBox();
-		RecoveryBox bigRBox = new RecoveryBox(chartRecovery);
-		VBox recoveryBox = bigRBox.getRecoveryBox();
+		
 
 		miExit.setOnAction(e -> {
 			Platform.exit();
-		});
-
-		miConfirmedCases.setOnAction(e -> {
-			root.setCenter(confirmedBox);
-		});
-
-		miDeaths.setOnAction(e -> {
-			root.setCenter(deathBox);
-		});
-
-		miRecoveries.setOnAction(e -> {
-			root.setCenter(recoveryBox);
 		});
 
 		miCDC.setOnAction(e -> {
@@ -92,17 +61,10 @@ public class MyMenuBar extends MenuBar {
 		});
 
 		menuFile.getItems().add(miExit);
-		menuView.getItems().addAll(miConfirmedCases, miDeaths, miRecoveries);
+		menuView.getItems().addAll(menuCorona);
 		menuHelp.getItems().addAll(menuGithub, miCDC, miSources, miDonate, miAbout);
 	}
 
-	public LineChart makeChart() {
-		NumberAxis xAxis = new NumberAxis();
-		xAxis.setLabel("Days");
-		NumberAxis yAxis = new NumberAxis();
-		yAxis.setLabel("Cases");
-		LineChart chart = new LineChart(xAxis, yAxis);
-		return chart;
-	}
+	
 
 }
