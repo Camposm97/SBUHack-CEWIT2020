465f97ab4ea7128d30b613936624707cedba40e1
Camposm97 <camposm1205@gmail.com>
2020-02-15 16:52:32 -0500

    More Formatting

--- src/model/CoronaData.java
+++ src/model/CoronaData.java
@@ -8,7 +8,7 @@ public class CoronaData {
 	private String latitude;
 	private String longitude;
 	private LinkedList<Integer> deathsConfirmedOrRecovered;
-	
+
 	public CoronaData(String provinceOrState, String countryOrRegion, String latitude, String longitude,
 			LinkedList<Integer> numberOfDeaths) {
 		super();
@@ -18,37 +18,45 @@ public class CoronaData {
 		this.longitude = longitude;
 		this.deathsConfirmedOrRecovered = numberOfDeaths;
 	}
+
 	public String getProvinceOrState() {
 		return provinceOrState;
 	}
+
 	public void setProvinceOrState(String provinceOrState) {
 		this.provinceOrState = provinceOrState;
 	}
+
 	public String getCountryOrRegion() {
 		return countryOrRegion;
 	}
+
 	public void setCountryOrRegion(String countryOrRegion) {
 		this.countryOrRegion = countryOrRegion;
 	}
+
 	public String getLatitude() {
 		return latitude;
 	}
+
 	public void setLatitude(String latitude) {
 		this.latitude = latitude;
 	}
+
 	public String getLongitude() {
 		return longitude;
 	}
+
 	public void setLongitude(String longitude) {
 		this.longitude = longitude;
 	}
+
 	public LinkedList<Integer> getDeathsConfirmedOrRecovered() {
 		return deathsConfirmedOrRecovered;
 	}
+
 	public void setDeathsConfirmedOrRecovered(LinkedList<Integer> numberOfDeaths) {
 		this.deathsConfirmedOrRecovered = numberOfDeaths;
 	}
-	
-	
-	
+
 }
