9c2ea6a40559b4cb800c3b791d13d92e32f57c33
Camposm97 <camposm1205@gmail.com>
2020-02-15 14:16:08 -0500

    Created CamposDemo.java

---
+++ src/app/CamposDemo.java
@@ -0,0 +1,32 @@
+package app;
+
+import java.net.URL;
+import java.util.Scanner;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class CamposDemo {
+	private static final int VALID_RESPONSE_CODE = 200;
+
+	public static void main(String[] args) throws Exception {
+		String s = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/archived_data/time_series/time_series_2019-ncov-Deaths.csv";
+		
+		String inLine = "";
+		URL url = new URL(s);
+		HttpsURLConnection c = (HttpsURLConnection) url.openConnection();
+		c.setRequestMethod("GET");
+		c.connect();
+		int responseCode = c.getResponseCode();
+		if (responseCode != VALID_RESPONSE_CODE) {
+			throw new RuntimeException("HttpResponseCode: " + responseCode);
+		} else {
+			System.out.println("Valid Response Code: " + responseCode);
+			Scanner sc = new Scanner(url.openStream());
+			while (sc.hasNext()) {
+				inLine = sc.nextLine();
+				System.out.println(inLine.replaceAll(",", ", "));
+			}
+		}
+	}
+
+}
