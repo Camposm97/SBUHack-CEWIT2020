dd6a2620efcd3ada402cee12ad2eb0c8c4e55619
Camposm97 <camposm1205@gmail.com>
2020-02-14 23:05:39 -0500

    Control + Shift + F

--- src/util/DataImport.java
+++ src/util/DataImport.java
@@ -9,13 +9,13 @@ import model.CoronaVirusInfo;
 
 public class DataImport {
 	public static LinkedList<CoronaVirusInfo> importCoronaData(File importFile) {
-		File f= importFile;
+		File f = importFile;
 		Scanner in;
-		LinkedList<CoronaVirusInfo> theList= new LinkedList<>();
+		LinkedList<CoronaVirusInfo> theList = new LinkedList<>();
 		try {
-			in= new Scanner(f);
+			in = new Scanner(f);
 		} catch (FileNotFoundException e) {
-			in=null;
+			in = null;
 			e.printStackTrace();
 		}
 		String province;
@@ -24,45 +24,45 @@ public class DataImport {
 		int confirmedCases;
 		int deaths;
 		int recovered;
-		while(in.hasNextLine()) {
-			String currentLine=in.nextLine();
-			String[] tokens= currentLine.split(",");
-			if(currentLine.charAt(0)==',') {
-				province="none";
+		while (in.hasNextLine()) {
+			String currentLine = in.nextLine();
+			String[] tokens = currentLine.split(",");
+			if (currentLine.charAt(0) == ',') {
+				province = "none";
 			}
-			if(currentLine.length()==3) {
-				province=tokens[0];
-				stateOrCountry=tokens[1];
-				lastUpdate=tokens[2];
-				confirmedCases=0;
-				deaths=0;
-				recovered=0;
+			if (currentLine.length() == 3) {
+				province = tokens[0];
+				stateOrCountry = tokens[1];
+				lastUpdate = tokens[2];
+				confirmedCases = 0;
+				deaths = 0;
+				recovered = 0;
 			}
-			if(currentLine.length()==4) {
-				province=tokens[0];
-				stateOrCountry=tokens[1];
-				lastUpdate=tokens[2];
-				confirmedCases=Integer.parseInt(tokens[3]);
-				deaths=0;
-				recovered=0;
+			if (currentLine.length() == 4) {
+				province = tokens[0];
+				stateOrCountry = tokens[1];
+				lastUpdate = tokens[2];
+				confirmedCases = Integer.parseInt(tokens[3]);
+				deaths = 0;
+				recovered = 0;
 			}
-			if(currentLine.length()==5) {
-				province=tokens[0];
-				stateOrCountry=tokens[1];
-				lastUpdate=tokens[2];
-				confirmedCases=Integer.parseInt(tokens[3]);
-				deaths=Integer.parseInt(tokens[4]);
-				recovered=0;
+			if (currentLine.length() == 5) {
+				province = tokens[0];
+				stateOrCountry = tokens[1];
+				lastUpdate = tokens[2];
+				confirmedCases = Integer.parseInt(tokens[3]);
+				deaths = Integer.parseInt(tokens[4]);
+				recovered = 0;
+			} else {
+				province = tokens[0];
+				stateOrCountry = tokens[1];
+				lastUpdate = tokens[2];
+				confirmedCases = Integer.parseInt(tokens[3]);
+				deaths = Integer.parseInt(tokens[4]);
+				recovered = Integer.parseInt(tokens[5]);
 			}
-			else {
-				province=tokens[0];
-				stateOrCountry=tokens[1];
-				lastUpdate=tokens[2];
-				confirmedCases=Integer.parseInt(tokens[3]);
-				deaths=Integer.parseInt(tokens[4]);
-				recovered=Integer.parseInt(tokens[5]);
-			}
-			CoronaVirusInfo cvi= new CoronaVirusInfo(province,stateOrCountry,lastUpdate,confirmedCases,deaths,recovered);
+			CoronaVirusInfo cvi = new CoronaVirusInfo(province, stateOrCountry, lastUpdate, confirmedCases, deaths,
+					recovered);
 			theList.add(cvi);
 		}
 		return theList;
