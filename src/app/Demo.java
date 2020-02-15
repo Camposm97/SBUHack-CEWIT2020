889faf0d033cf63b77e4744a4fe0ad60d359fec2
Camposm97 <camposm1205@gmail.com>
2020-02-15 16:43:08 -0500

    Renamed a variable

--- src/app/Demo.java
+++ src/app/Demo.java
@@ -8,8 +8,8 @@ import util.DataParser;
 public class Demo {
 
 	public static void main(String[] args) throws IOException {
-		CoronaDatabase saveCorona = DataParser.importCorona();
-		System.out.println(saveCorona.getCoronaConfirmed().get(10).getDeathsConfirmedOrRecovered());
+		CoronaDatabase coronaDb = DataParser.importCorona();
+		System.out.println(coronaDb.getCoronaConfirmed().get(10).getDeathsConfirmedOrRecovered());
 	}
 
 }
