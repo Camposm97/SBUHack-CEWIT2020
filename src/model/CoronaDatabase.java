8d4a6a6c7d626528cd6ec4dca8d2a57ec2b49152
Camposm97 <camposm1205@gmail.com>
2020-02-15 23:54:57 -0500

    Implementing methods to access database easily

--- src/model/CoronaDatabase.java
+++ src/model/CoronaDatabase.java
@@ -1,7 +1,9 @@
 package model;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class CoronaDatabase {
 	private List<CoronaData> coronaDeaths;
@@ -26,40 +28,57 @@ public class CoronaDatabase {
 	public List<CoronaData> getCoronaConfirmed() {
 		return coronaConfirmed;
 	}
+
+	public List<String> getCountriesInfected() {
+		Set<String> hashSet = new HashSet<>();
+		for (CoronaData cd : coronaDeaths) {
+			hashSet.add(cd.getCountryOrRegion());
+		}
+		for (CoronaData cd : coronaRecovered) {
+			hashSet.add(cd.getCountryOrRegion());
+		}
+		for (CoronaData cd : coronaConfirmed) {
+			hashSet.add(cd.getCountryOrRegion());
+		}
+		List<String> list = new LinkedList<>(hashSet);
+		return list;
+	}
 	
+	
+
 	public List<CoronaData> searchDeathsByArea(String key) {
 		List<CoronaData> resultList = new LinkedList<>();
 		for (int i = 0; i < coronaDeaths.size(); i++) {
-			if (coronaDeaths.get(i).getCountryOrRegion().toLowerCase().contains(key.toLowerCase()) ||
-					coronaDeaths.get(i).getProvinceOrState().toLowerCase().contains(key.toLowerCase())) {
+			if (coronaDeaths.get(i).getCountryOrRegion().toLowerCase().contains(key.toLowerCase())
+					|| coronaDeaths.get(i).getProvinceOrState().toLowerCase().contains(key.toLowerCase())) {
 				resultList.add(coronaDeaths.get(i));
 			}
 		}
 		return resultList;
 	}
-	
+
 	public List<CoronaData> searchRecoveredByArea(String key) {
 		List<CoronaData> resultList = new LinkedList<>();
 		for (int i = 0; i < coronaRecovered.size(); i++) {
-			if (coronaRecovered.get(i).getCountryOrRegion().toLowerCase().contains(key.toLowerCase()) ||
-					coronaRecovered.get(i).getProvinceOrState().toLowerCase().contains(key.toLowerCase())) {
+			if (coronaRecovered.get(i).getCountryOrRegion().toLowerCase().contains(key.toLowerCase())
+					|| coronaRecovered.get(i).getProvinceOrState().toLowerCase().contains(key.toLowerCase())) {
 				resultList.add(coronaRecovered.get(i));
 			}
 		}
 		return resultList;
 	}
-	
+
 	public List<CoronaData> searchConfirmedByArea(String key) {
 		List<CoronaData> resultList = new LinkedList<>();
 		for (int i = 0; i < coronaConfirmed.size(); i++) {
-			if (coronaConfirmed.get(i).getCountryOrRegion().toLowerCase().contains(key.toLowerCase()) ||
-					coronaRecovered.get(i).getProvinceOrState().toLowerCase().contains(key.toLowerCase())) {
+			if (coronaConfirmed.get(i).getCountryOrRegion().toLowerCase().contains(key.toLowerCase())
+					|| coronaRecovered.get(i).getProvinceOrState().toLowerCase().contains(key.toLowerCase())) {
 				resultList.add(coronaConfirmed.get(i));
 			}
 		}
 		return resultList;
 	}
-	
+
 	public int getTotalDeaths() {
 		int count = 0;
 		for (CoronaData cd : coronaDeaths) {
@@ -67,7 +86,7 @@ public class CoronaDatabase {
 		}
 		return count;
 	}
-	
+
 	public int getTotalRecovered() {
 		int count = 0;
 		for (CoronaData cd : coronaRecovered) {
@@ -75,7 +94,7 @@ public class CoronaDatabase {
 		}
 		return count;
 	}
-	
+
 	public int getTotalConfirmed() {
 		int count = 0;
 		for (CoronaData cd : coronaConfirmed) {
