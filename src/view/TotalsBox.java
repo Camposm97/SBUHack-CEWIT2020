b1b6cbebb8442e196b52e849b1934c8a1a4896ae
Jonathan Lemus <lemusj27@hotmail.com>
2020-02-15 22:03:23 -0500

    Removed unused code in TotalsBox, fixed MenuCorona's BarChart

--- src/view/TotalsBox.java
+++ src/view/TotalsBox.java
@@ -19,42 +19,13 @@ import util.DataParser;
 
 public class TotalsBox {
 	
-	private VBox comparisonBox;
-	private VBox box2;
-	private HBox boxs;
-	private Label title;
+	private HBox comparisonBox;
 	
 	public TotalsBox(BarChart totals) {
 
-		comparisonBox = new VBox();
+		comparisonBox = new HBox();
 		comparisonBox.setAlignment(Pos.CENTER);
 		
-//		List<CoronaData> listC = App.DB.getCoronaConfirmed();
-//		List<CoronaData> listD = App.DB.getCoronaDeaths();
-//		List<CoronaData> listR = App.DB.getCoronaRecovered();
-//		XYChart.Series<String, Number> seriesC = new XYChart.Series<>();
-//		XYChart.Series<String, Number> seriesD = new XYChart.Series<>();
-//		XYChart.Series<String, Number> seriesR = new XYChart.Series<>();
-//		for(CoronaData corona: listC) {
-//			LinkedList<Integer> countListC = corona.getCountList();
-//			for(int i=0; i<countListC.size(); i++) {
-//				seriesC.getData().add(new Data<String, Number>("Confirmed", countListC.get(i)));
-//			}
-//		}
-//		
-//		for(CoronaData corona: listD) {
-//			LinkedList<Integer> countListD = corona.getCountList();
-//			for(int i=0; i<countListD.size(); i++) {
-//				seriesD.getData().add(new Data<String, Number>("Deaths", countListD.get(i)));
-//			}
-//		}
-//		
-//		for(CoronaData corona: listR) {
-//			LinkedList<Integer> countListR = corona.getCountList();
-//			for(int i=0; i<countListR.size(); i++) {
-//				seriesR.getData().add(new Data<String, Number>(i, countListR.get(i)));
-//			}
-//		}
 		int totalC = App.DB.getTotalConfirmed();
 		int totalD = App.DB.getTotalDeaths();
 		int totalR = App.DB.getTotalRecovered();
@@ -64,9 +35,10 @@ public class TotalsBox {
 		totalSeries.getData().add(new Data<String, Number>("Recovered", totalR));
 		totals.getData().add(totalSeries);
 		comparisonBox.getChildren().add(totals);
+		comparisonBox.setMinHeight(600);
 	}
 	
-	public VBox getTotalsBox() {
+	public HBox getTotalsBox() {
 		return comparisonBox;
 	}
 }
