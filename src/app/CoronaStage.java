98185705554e93a31f439f910362db66509dc253
Camposm97 <camposm1205@gmail.com>
2020-02-24 00:40:58 -0500

    Working on CoronaStage

--- src/app/CoronaStage.java
+++ src/app/CoronaStage.java
@@ -1,4 +1,17 @@
 package app;
 
-public class CoronaStage {
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import view.MyMenuBar;
+
+public class CoronaStage extends Stage {
+    private MyMenuBar menuBar;
+    private BorderPane root;
+
+    public CoronaStage() {
+        this.root = new BorderPane();
+        this.menuBar = new MyMenuBar(root);
+        this.setTitle(App.TITLE);
+
+    }
 }
